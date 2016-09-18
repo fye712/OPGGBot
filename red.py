7f8de9b0bc1131d68d61611f56e39aa309fb79ff
Franklin Ye <fye7.12@gmail.com>
2016-09-17 21:05:58 -0500

    very basic working version for test subreddit

--- red.py
+++ red.py
@@ -6,7 +6,7 @@ from config_bot import *
 
 
 
-user_agent = ("Python OPGG Bot 0.1 (by /u/franklindaking)")
+user_agent = ("Python OPGG Linker 0.1 (by /u/franklindaking)")
 r = praw.Reddit(user_agent = user_agent)
 r.login(REDDIT_USERNAME, REDDIT_PASSWORD)
 
@@ -18,12 +18,22 @@ else:
         posts_replied_to = posts_replied_to.split("\n")
         posts_replied_to = filter(None, posts_replied_to)
 
-subreddit = r.get_subreddit("summonerschool")
-for submission in subreddit.get_hot(limit = 50):
-    if submission.id not in posts_replied_to:
-        flat_comments = praw.helpers.flatten_tree(submission.comments)
-        for comment in flat_comments:
-            for match in re.finditer('<(.*?)>'):
+subreddit = r.get_subreddit("test")
+
+for submission in subreddit.get_hot(limit = 5):
+    flat_comments = praw.helpers.flatten_tree(submission.comments)
+    for comment in flat_comments:
+        if comment.id not in posts_replied_to:
+            for match in re.findall('<(.*?)>', comment.body):
                 # build the response?
+                response = "na.op.gg/summoner/userName=" + match
+                comment.reply(response)
+                print "Bot replying to : " + submission.title
+                posts_replied_to.append(comment.id)
+
+    with open("posts_replied_to.txt", "w") as f:
+        for post_id in posts_replied_to:
+            f.write(post_id + "\n")
+
 
 
